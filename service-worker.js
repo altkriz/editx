3497b717fa59d59793ca01ee23872148dfb26af4
Kashif Raza <k.raza1932000@gmail.com>
2026-02-16 00:59:48 +0530

    Change asset URLs to HTTPS
    Updated asset URLs to use HTTPS for better security.

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'editx-v1';
 const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json',
+    'https://altkriz.github.io/editx/',
+    'https://altkriz.github.io/editx/index.html',
+    'https://altkriz.github.io/editx/manifest.json',
     'https://cdn.tailwindcss.com',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
     'https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/selfie_segmentation.js'
